2a32ee615dd192ed3133569e8b230d8d028fbcbb
BOOZF <97935723+BOOZF@users.noreply.github.com>
2023-12-19 11:45:26 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cfh3lxwfw6nf.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "boozhengfeng-employee"
+customregion = "us-east-1"
+
